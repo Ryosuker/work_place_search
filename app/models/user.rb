1888e1cee90aa7691c6e59e02e9e2d94ad2c222b
Ryosuker <ryowada290@gmail.com>
2024-10-21 17:39:03 +0900

    管理者メソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
 
   validates :password, format: { with: PASSSWORD_REGEX, message: 'は6文字以上で、半角英字と半角数字の両方を含めてください' }
   validates :nickname, presence: true
+
+  # 管理者かどうかを判定するメソッド
+  def admin?
+    role == "admin"
+  end
+  # 一般ユーザーかどうかを判定するメソッド（オプション）
+  def user?
+    role == "user"
+  end
 end
